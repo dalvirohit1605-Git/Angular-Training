b1a34aa6b4cf6186d0e1c44c13e62d03dbe3bdda
FPS\rohit.dalvi <rohit.dalvi>
2025-10-31 10:05:57 +0530

    new changes

--- estore/src/app/membership/register/register.component.ts
+++ estore/src/app/membership/register/register.component.ts
@@ -11,10 +11,46 @@ import { CommonModule } from '@angular/common';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-    model = new Customer('Ravi','Tambade','ravi.tambade@transflower.in',9881735801,28,
-                     new Date(1975,18,8),'Pune',
-                     'S',
-                     false,['T', 'B']);
-    constructor() {  }
+   
+  submitted = false;
+  locations = ['Pune','Mumbai','Delhi','Bangalore','Nashik','Chennai'];
+  memberShips = [
+                 {value:'G',display:'Gold'},
+                 {value:'S',display:'Silver'},
+                 {value:'P',display:'Platinum'}
+  ];
+ 
+  social =[
+             {value:'T',display:'Twitter'},
+             {value:'F',display:'Facebook'},
+             {value:'B',display:'Blog'},
+             {value:'I',display:'Instagram'},
+             {value:'L',display:'LinkedIn'}
+  ];
+ 
+ 
+  socialStatus = ['T', 'B'];
+
+
+  model=new Customer('Ravi','Tambade','ravi.tambade@transflower.in',9881735801,28,
+                     new Date(1975,18,8),this.locations[0],
+                     this.memberShips[1].value,
+                     false,this.socialStatus);
+   constructor() {  }
+ 
+ 
+  onSubmit(form: any): void {
+       this.submitted=true;
+       console.log('you submitted value:', form);
+  }
+ 
+  showCustomer(form:any){
+         return form && form.controls['firstName'] && form.controls['firstName'].value;
+  }
+ 
+  showFormControls(form:any){
+     return form && form.controls['firstName'] && form.controls['firstName'].value;
+  }
+
 
 }
--- estore/src/app/membership/sign-in/sign-in.component.ts
+++ estore/src/app/membership/sign-in/sign-in.component.ts
@@ -19,7 +19,14 @@ export class SignInComponent {
   isValidUser:boolean=false;
   user: Credential=new Credential("ravi.tambade@transflower.in","seed");
  
+  //Constructor 
   constructor(private svc:AuthService) {    }  //DI
 
  
+
+  onSubmit(form: any): void {
+   this.isValidUser=this.svc.validate(form.userEmail,form.userPassword);
+   if(this.isValidUser){ console.log("Valid User !"); }
+   else{ console.log("Invalid User !"); }   
+  }
 }
